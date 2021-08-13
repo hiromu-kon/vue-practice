0963d096fd6cafec178e82cd5c5a9a72b2cdd2ef
hiromu-kon <hiro.0405soccer@icloud.com>
2021-08-13 21:54:25 +0900

    first commit

---
+++ first-lesson/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div style="width: 700px; margin: auto; padding-top: 50px;">
+    <Home></Home>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Home from "./views/Home.vue";
+export default {
+  components: {
+    Home
+  }
+}
+</script>
+
+<style scoped>
+  .link {
+    margin-right: 10px;
+  }
+  .link--active {
+    font-size: 20px;
+  }
+</style>
---
+++ first-lesson/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3>Home</h3>
+    <button @click="toUsers">Usersのページに行く</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  methods: {
+    toUsers() {
+      this.$router.push({
+        name: 'user-id-profile', 
+        params: { id: 1 }
+      });
+    }
+  }
+};
+</script>
